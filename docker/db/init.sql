d9d78b5dfe82e35a9f8af0473ec664fcd49cdcc0
Alexandra Gonzalez <139160786+AlexGonzalez99@users.noreply.github.com>
2024-01-29 15:47:58 -0500

    Update init.sql
    updated fields for books table and added user table.

--- docker/db/init.sql
+++ docker/db/init.sql
@@ -3,5 +3,24 @@ CREATE TABLE books (
     id SERIAL PRIMARY KEY,
     title varchar(128),
     author varchar(128),
+    isbn int, 
+    publicationdate date, 
+    genre varchar(30),
+    pages int, 
+    language varchar(30)
     file_path text
 );
+
+CREATE TABLE user(
+    UserName varchar(30),
+	Password varchar(30),
+	LastName varchar(30),
+	FirstName varchar(30),
+	CreditCardNum int,
+	CreditCardExp varchar(5),
+	CCV int,
+	BillingAddress varchar(92),
+	BillingZip int, 
+	BillingCity varchar(30), 
+	BillingState varchar(30)
+    );
